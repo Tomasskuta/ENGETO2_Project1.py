c67885b8f3ff05bf23a78293d78063206fc3b908
Tomasskuta <tomasskuta@seznam.cz>
2025-04-28 15:46:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
+ukoly = []
+
 def hlavni_menu():
-    volba_menu = input("Vyberte možnost (1-4):")
-    if volba_menu == 1:
-        pridat_ukol()
-    elif volba_menu == 2:
-        zobrazit_ukoly()
-    elif volba_menu == 3:
-        odstranit_ukol()
-    else:
-        exit()
+    while True:
+        volba_menu = int(input("Vyberte možnost (1-4): "))
+        if volba_menu in range(1,5):
+            if volba_menu == 1:
+                pridat_ukol()
+            elif volba_menu == 2:
+                zobrazit_ukoly()
+            elif volba_menu == 3:
+                odstranit_ukol()
+            else:
+                exit()
+        else:
+            print("Volba musí být v rozmezí 1-4! ")
+            
 
 def pridat_ukol():
-    print("pridat")
+    nazev_ukol = input("Zadejte název úkolu: ")
+    popis_ukol = input("Zadejte popis úkolu: ")
+    ukol = nazev_ukol + (" - ") + popis_ukol
+
+    ukoly.append(ukol)
+    print("Úkol " +nazev_ukol +" byl přidán.")
 
 def zobrazit_ukoly():
-    print("zobrazit")
+    i = 1
+    for ukol in ukoly:
+        i = +i
+        print(str(i) + (". ") + ukol)
     
 def odstranit_ukol():
-    print("odstranit")
+    vymaz_ukol = int(input("Zadejte číslo úkolu, který chcete odstranit: ")) - 1
+    
+    ukoly.pop(vymaz_ukol)
 
 
 hlavni_menu()
